862d8da7d7893199d13534e1c17330d9a2b1c4cd
Quan Trinh <trinhquan1123@yahoo.com>
2016-08-08 18:49:26 +0100

    added solar panel model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160808145202) do
+ActiveRecord::Schema.define(version: 20160808174109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "solar_panels", force: :cascade do |t|
+    t.integer  "size"
+    t.float    "efficiency"
+    t.float    "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -38,4 +47,5 @@ ActiveRecord::Schema.define(version: 20160808145202) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "solar_panels", "users"
 end
