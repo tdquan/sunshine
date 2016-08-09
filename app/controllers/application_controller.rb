f8b6300889a792752d13cb4896d435592cf658f8
ricardo serrao <ricardo.spsantos@gmail.com>
2016-08-09 20:28:01 +0100

    link to dashboard page when logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def after_sign_in_path_for(user)
-    edit_user_registration_path
+    users_dashboard_path
   end
 end
