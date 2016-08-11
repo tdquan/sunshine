f3c533009cdcb6dd3e5e5d5b413d71733333bd6c
richard lumpi <richard.lumpi@sbg.at>
2016-08-11 17:41:55 +0100

    solar_panel model geocoded

--- app/models/solar_panel.rb
+++ app/models/solar_panel.rb
@@ -5,5 +5,9 @@ class SolarPanel < ActiveRecord::Base
 
   validates :price, presence: true
   validates :size, presence: true
+  validates :address, presence: true
   validates_uniqueness_of :user_id
+
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
 end
