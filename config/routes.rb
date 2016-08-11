dbe78940b3af958764cb3f12e74a184c52ee47c6
Quan Trinh <trinhquan1123@yahoo.com>
2016-08-11 04:24:18 +0100

    card display for solar panels

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   get "/current_user/solar_panels" => "solar_panels#index", as: "user_solar_panels"
   get "/current_user/solar_panels/:id" => "solar_panels#show", as: "show_solar_panel"
+
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
