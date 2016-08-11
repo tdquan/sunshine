0475c2e0074b7515328b68ea34baa5ab652040ad
Thomas Carrier <hthomascarrier@gmail.com>
2016-08-11 12:30:58 +0100

    validation added for contracts and solar_panel

--- app/models/solar_panel.rb
+++ app/models/solar_panel.rb
@@ -1,4 +1,9 @@
 class SolarPanel < ActiveRecord::Base
   # Prosumer
   belongs_to :user
+
+  validates :price, presence: true
+  validates :size, presence: true
+  validates_uniqueness_of :user_id
+
 end
