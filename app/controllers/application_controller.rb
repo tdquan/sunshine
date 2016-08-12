25b77cbf0e5c09ffb198799a32d70d06f99d3577
richard lumpi <richard.lumpi@sbg.at>
2016-08-12 01:55:48 +0100

    little improvements

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,15 @@ class ApplicationController < ActionController::Base
     user_dashboard_path
   end
 
+  def after_update_path_for(user)
+    user_dashboard_path
+  end
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address])
   end
 end
 
