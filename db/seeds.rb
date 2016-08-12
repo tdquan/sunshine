8c0294f8be5fec7686e443d2fc7825260868a040
lumpirichard <richard.lumpi@sbg.at>
2016-08-12 17:06:11 +0100

    added seeds and problem with map

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,77 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.create(
+  email: "janet@gmail.com",
+  first_name: "Janet",
+  last_name: "Marshall",
+  family_size: 3,
+  apartment_size: 100,
+  address: "676 President St Brooklyn, NY 11215",
+  password: "123456",
+  password_confirmation: "123456"
+  )
+
+User.create(
+  email: "bob@gmail.com",
+  first_name: "Bob",
+  last_name: "Jackson",
+  family_size: 1,
+  apartment_size: 50,
+  address: "675 President St Brooklyn, NY 11215",
+  password: "123456",
+  password_confirmation: "123456"
+  )
+
+User.create(
+  email: "maxime@gmail.com",
+  first_name: "Maxime",
+  last_name: "Longden",
+  family_size: 2,
+  apartment_size: 60,
+  address: "20 Rua Guarda-Mor",
+  password: "123456",
+  password_confirmation: "123456"
+  )
+
+SolarPanel.create(
+  size: 10,
+  efficiency: 3,
+  price: 2,
+  address: "676 President St Brooklyn, NY 11215",
+  user_id: 1,
+  )
+
+SolarPanel.create(
+  size: 8,
+  efficiency: 2,
+  price: 1,
+  address: "675 President St Brooklyn, NY 11215",
+  user_id: 2,
+  )
+
+  SolarPanel.create(
+  size: 7,
+  efficiency: 4,
+  price: 2,
+  address: "20 Rua Guarda-Mor",
+  user_id: 3,
+  )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
