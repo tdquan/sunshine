a6a8af6bb1cbf656d549cbfbbf36a41206a3416d
lumpirichard <richard.lumpi@sbg.at>
2016-08-16 16:15:19 +0100

    improved style of hover box

--- app/models/solar_panel.rb
+++ app/models/solar_panel.rb
@@ -4,6 +4,8 @@ class SolarPanel < ActiveRecord::Base
   has_many :contracts
 
   validates :price, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validates :size, numericality: { greater_than: 0 }
   validates :size, presence: true
   validates :address, presence: true
   validates_uniqueness_of :user_id
