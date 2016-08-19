70c7bacdbf169afbef445d377cee173b8eafec82
lumpirichard <richard.lumpi@sbg.at>
2016-08-19 17:00:58 +0100

    dashboard has 3 cards now

--- app/assets/javascripts/money_counter.js
+++
@@ -1,8 +0,0 @@
-function digitCheck(x,y){
-  var digitsOld = splitToArray(x),
-  digitsNew = splitToArray(y);
-  for (var i = 0, c = digitsNew.length; i < c; i++){
-    if (digitsNew[i] != digitsOld[i]){
-      animateDigit(i, digitsOld[i], digitsNew[i]);
-    }
-  }
