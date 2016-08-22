653b7de761836b37d7e3741ad1e18ce672ff17d9
Quan Trinh <trinhquan1123@yahoo.com>
2016-08-22 19:17:51 +0100

    added seeds

--- app/jobs/fetch_usage_job.rb
+++ app/jobs/fetch_usage_job.rb
@@ -14,6 +14,15 @@ class FetchUsageJob < ActiveJob::Base
             puts "#{p.time} : #{a} : #{v}"
             # Check if current user has a solar_panel
             if user.solar_panel !nil
+              # For testing purpose
+              ProductionPattern.all.each do |p|
+                p.solar_panel_id = user.solar_panel.id
+                p.save
+              end
+
+              # End of testing
+
+
               prod = FetchProductionJob.perform_now(user.solar_panel.id, p.time, a)
               if prod > v
                 puts "The excess is #{(prod - v).round(2)}"
