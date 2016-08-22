3149026828a5a801bbe458434afc638784e1766e
lumpirichard <richard.lumpi@sbg.at>
2016-08-22 19:42:59 +0100

    put transactions into contract show view

--- db/schema.rb
+++ db/schema.rb
@@ -136,12 +136,12 @@ ActiveRecord::Schema.define(version: 20160822172830) do
     t.string   "token"
     t.datetime "token_expiry"
     t.string   "address"
-    t.float    "latitude"
-    t.float    "longitude"
-    t.integer  "premium"
-    t.integer  "bill"
     t.integer  "consumption_pattern_id"
     t.integer  "production_pattern_id"
+    t.integer  "premium"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.integer  "bill"
     t.string   "status"
   end
 
