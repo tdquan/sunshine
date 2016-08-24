4bc97ab2abcd683bd8e864b61f8bbf538f927f8d
Thomas Carrier <hthomascarrier@gmail.com>
2016-08-24 13:21:39 +0100

    fixed address

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,11 +1,9 @@
 class DashboardController < ApplicationController
   def show
-    current_user.update(address: session[:current_user_address])
     sign_in current_user, bypass: true
   end
 
   def show_neighbour
-    current_user.update(address: session[:current_user_address])
     sign_in current_user, bypass: true
   end
 end
