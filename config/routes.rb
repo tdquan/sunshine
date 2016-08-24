2cc19e3c5f6f32149d6b4d28760f363686460516
ricardo serrao <ricardo.spsantos@gmail.com>
2016-08-24 16:55:39 +0100

    error messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  # get 'errors/not_found'
+
+  # get 'errors/internal_server_error'
+
   require "sidekiq/web"
   authenticate :user, lambda { |u| u.status == "admin" } do
     mount Sidekiq::Web => '/sidekiq'
@@ -47,8 +51,8 @@ Rails.application.routes.draw do
 
   # ---------------   errors  ----------------------------
   
-  get "/404", :to => "errors#not_found", :via => :all
-  get "/500", :to => "errors#internal_server_error", :via => :all
+  match "/404", :to => "errors#not_found", :via => :all
+  match "/500", :to => "errors#internal_server_error", :via => :all
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
