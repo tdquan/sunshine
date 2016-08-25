10f855f70e5f07beaf4fc3488d34a183e7fc9c21
lumpirichard <richard.lumpi@sbg.at>
2016-08-25 13:47:13 +0100

    added layout to graphs

--- app/assets/javascripts/simulation_done_graph.js
+++ app/assets/javascripts/simulation_done_graph.js
@@ -4,11 +4,14 @@ var dps = $("#simulation-done-graph").data("points");
 console.log(dps)
     var chart = new CanvasJS.Chart("simulation-done-graph",{
       title :{
-        text: "Total Energy Transfer"
+        text: "Total Energy Transfer",
+        fontColor: "#F5A623",
       },
       data: [{
         type: "area",
-        dataPoints: dps
+        dataPoints: dps,
+        color: "#F5A623",
+        fillOpacity: .3,
       }],
       axisX:{
         viewportMinimum: 0,
@@ -17,7 +20,8 @@ console.log(dps)
       axisY:{
         viewportMinimum: 0,
         viewportMaximum: 15,
-      }
+      },
+       backgroundColor: ""
     });
     chart.render();
 });
